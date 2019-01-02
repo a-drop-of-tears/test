5edb4ae0823bfc7c86bb3f2ec910d21f865842a5
a-drop-of-tears <2020485908@qq.com>
2019-01-02 14:50:38 +0800

    two commit

---
+++ src/test.java
@@ -0,0 +1,11 @@
+/**
+ * test
+ *
+ * @author Mr Li
+ * @date 2019/1/2
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
---
+++ src/testTwo.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+/**
+ * testTwo
+ *
+ * @author Mr Li
+ * @date 2019/1/2
+ */
+public class testTwo implements Serializable {
+}
